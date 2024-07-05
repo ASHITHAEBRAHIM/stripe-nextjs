d522ba7ce63ad7b497b7c80237c0d9e993674d35
Ashitha Ebrahim <achuashitha5@gmail.com>
2024-07-05 11:22:06 +0530

    updated paymentlinks

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,18 +3,18 @@ export const constants = {
     product1:
       process.env.NODE_ENV === "development"
         ? "https://buy.stripe.com/test_eVaeVNd3b2We92g144"
-        : "",
+        : "https://buy.stripe.com/test_eVaeVNd3b2We92g144",
     product2:
       process.env.NODE_ENV === "development"
         ? " https://buy.stripe.com/test_4gw7tlfbj40i2DS3cd"
-        : "",
+        : "https://buy.stripe.com/test_4gw7tlfbj40i2DS3cd",
     product3:
       process.env.NODE_ENV === "development"
         ? "https://buy.stripe.com/test_fZe7tl5AJ9kCemA002"
-        : "",
+        : "https://buy.stripe.com/test_fZe7tl5AJ9kCemA002",
     product4:
       process.env.NODE_ENV === "development"
         ? "https://buy.stripe.com/test_bIY3d53sBaoGfqE28b"
-        : "",
+        : "https://buy.stripe.com/test_bIY3d53sBaoGfqE28b",
   },
 };
